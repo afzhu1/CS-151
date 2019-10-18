8e1d1cde0f90112194e0f44ea1618deaf7c50e29
Adam Zhu <54492940+afzhu1@users.noreply.github.com>
2019-10-18 14:38:50 -0700

    finished question2

---
+++ MT 2/src/question2.java
@@ -0,0 +1,68 @@
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+   A program that allows users to edit a scene composed
+   of items.
+*/
+public class question2
+{
+	
+	
+   public static void main(String[] args)
+   {
+      JFrame frame = new JFrame();
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      final SceneComponent scene = new SceneComponent();
+
+      JButton animateButton = new JButton("Animate");
+      
+      animateButton.addActionListener(event -> scene.toggleAnimation());
+
+      JButton carButton = new JButton("Car");
+      carButton.addActionListener(new
+         ActionListener()
+         {
+            public void actionPerformed(ActionEvent event)
+            {
+               scene.add(new CarShape(20, 20, 50));
+            }
+         });
+
+      JButton removeButton = new JButton("Remove");
+      removeButton.addActionListener(new
+         ActionListener()
+         {
+            public void actionPerformed(ActionEvent event)
+            {
+               scene.removeSelected();
+            }
+         });
+
+      JPanel buttons = new JPanel();
+      buttons.add(animateButton);
+      buttons.add(carButton);
+      buttons.add(removeButton);
+
+      frame.add(scene, BorderLayout.CENTER);
+      frame.add(buttons, BorderLayout.NORTH);
+
+      frame.setSize(300, 300);
+      frame.setVisible(true);
+      
+      
+      Timer t = new Timer(100, event ->
+      {
+         if (scene.isAnimated() == true) {
+        	 scene.moveSelected();
+         }
+         
+      });
+      t.start();
+   }
+}
+
+
