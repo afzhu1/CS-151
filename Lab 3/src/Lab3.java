f19f76eb6e938a1ec06e479f5cee35bd208344f9
Adam Zhu <54492940+afzhu1@users.noreply.github.com>
2019-10-28 11:43:20 -0700

    prints out all objects

--- Lab 3/src/Lab3.java
+++ Lab 3/src/Lab3.java
@@ -1,14 +1,24 @@
-import java.util.*;
+import java.lang.reflect.Array;
+
 
 public class Lab3 {
 	
 
-	public static <E> void dumpArray(E[] list) {
-		for(int i = 0; i < list.length; i++) {
-			if(list[i] instanceof Object) {
-				System.out.println(list[i].toString());
+	public static void dumpArray(Object array) {
+		Class<?> arrayType = array.getClass();
+		if(arrayType.isArray()) {
+			Class<?> componentType = arrayType.getComponentType();
+
+			if(componentType instanceof Object) {
+				for(int i = 0; i < Array.getLength(array); i++) {
+					System.out.println(Array.get(array, i).toString());
 			}
+			}
+			
+		} else {
+			System.out.println("An array was not passed in.");
 		}
+		
 	}
 	
 	
@@ -19,14 +29,15 @@ public class Lab3 {
 		stringTest[2] = "2";
 		dumpArray(stringTest);
 		Integer[] integerTest = new Integer[3];
-		integerTest[0] = 1;
-		integerTest[1] = 2;
-		integerTest[2] = 3;
+		integerTest[0] = 3;
+		integerTest[1] = 4;
+		integerTest[2] = 5;
 		dumpArray(integerTest);
 		int[] intTest = new int[3];
-		intTest[0] = 1;
-		intTest[1] = 2;
-		intTest[2] = 3;
-	//	dumpArray(intTest);
+		intTest[0] = 6;
+		intTest[1] = 7;
+		intTest[2] = 8;
+		dumpArray(intTest);
+		dumpArray("String");
 	}
 }
