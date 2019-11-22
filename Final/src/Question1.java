0d3dc353c4750fbbe1abd021528781b73ad30d53
Adam Zhu <54492940+afzhu1@users.noreply.github.com>
2019-11-22 13:13:18 -0800

    separated file into two parts

--- Final/src/Question1.java
+++ Final/src/Question1.java
@@ -5,70 +5,9 @@ public class Question1 {
 
 		public static void main(String []args) {
 			Integer test = new Integer(3);
-			dumpClass(test.getClass());
-			dumpClass(test);
+			DumpClass.dumpClass(test.getClass());
+			DumpClass.dumpClass(test);
 		}
 	
-		public static void dumpClass(Class input) {
-			//Class input = input.getClass();
-			Class[] classInterfaces = input.getInterfaces();
-			Package classPackage = input.getPackage();
-			String packageName = classPackage.getName();
-			Field[] fields = input.getDeclaredFields();
-			Constructor[] constructors = input.getDeclaredConstructors();
-			Method[] methods =  input.getDeclaredMethods();
-			
-			
-			System.out.println("Class Name: " + "\n	" + input);
-			System.out.println("Package Name: " + "\n	" + classPackage.toString());
-			System.out.println("Superclass: " + "\n	" + input.getSuperclass());
-			System.out.println("Constructors: ");
-			
-			System.out.println("Fields: ");
-			for(Field f: fields) {
-				System.out.println("	" + f.toString());
-			}
-			
-			for(Constructor c: constructors) {
-				System.out.println("	" + c.toString());
-			}
-			System.out.println("Methods: ");
-			for(Method m: methods) {
-				System.out.println("	" + m.toString());
-			}
-			
-			
-			
-		}
 		
-		// @overload
-		public static void dumpClass(Object input) {
-			Class classType = input.getClass();
-			Class[] classInterfaces = classType.getInterfaces();
-			Package classPackage = classType.getPackage();
-			String packageName = classPackage.getName();
-			Field[] fields = classType.getDeclaredFields();
-			Constructor[] constructors = classType.getDeclaredConstructors();
-			Method[] methods =  classType.getDeclaredMethods();
-			
-			
-			System.out.println("Class Name: " + "\n	" + classType);
-			System.out.println("Package Name: " + "\n	" + classPackage.toString());
-			System.out.println("Superclass: " + "\n	" + classType.getSuperclass());
-			System.out.println("Fields: ");
-			for(Field f: fields) {
-				System.out.println("	" + f.toString());
-			}
-			System.out.println("Constructors: ");
-			for(Constructor c: constructors) {
-				System.out.println("	" + c.toString());
-			}
-			System.out.println("Methods: ");
-			for(Method m: methods) {
-				System.out.println("	" + m.toString());
-			}
-			
-			
-			
-		}
 }
