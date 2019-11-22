6999c7ead5173e5b38e6594edddea0f89f3363fb
Adam Zhu <54492940+afzhu1@users.noreply.github.com>
2019-11-22 13:29:58 -0800

    Update Question1.java

--- Final/src/Question1.java
+++ Final/src/Question1.java
@@ -1,12 +1,16 @@
 import java.lang.reflect.*;
-
+import java.util.*;
 
 public class Question1 {
 
 		public static void main(String []args) {
 			Integer test = new Integer(3);
+			ArrayList<Integer> integerArray = new ArrayList<>();
+			String stringTest = new String("yeet");
 			DumpClass.dumpClass(test.getClass());
 			DumpClass.dumpClass(test);
+			DumpClass.dumpClass(stringTest);
+			DumpClass.dumpClass(integerArray);
 		}
 	
 		
